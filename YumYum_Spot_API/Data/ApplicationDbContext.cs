d8585e0e6d259f0e1cc49bd51f7ece69abb3f065
nawonperera <nawon2794@gmail.com>
2025-12-02 21:52:30 +0530

    Added ASP.NET Core Identity with custom ApplicationUser, configured EF Core context, created migrations, and updated SQL Server settings.✏️

---
+++ YumYum_Spot_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using YumYum_Spot_API.Models;
+
+namespace YumYum_Spot_API.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+}
--- YumYum_Spot_API/Program.cs
+++ YumYum_Spot_API/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using YumYum_Spot_API.Data;
+using YumYum_Spot_API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +22,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
